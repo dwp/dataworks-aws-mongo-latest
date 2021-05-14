56bf7b3617087f9c710c3dd99c23fc205c1a000f
steveburton4 <Steven.burton@hotmail.co.uk>
2021-05-14 14:42:43 +0100

    Lambda output for Azkaban to use (#9)
    * Lambda output for Azkaban to use

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "mongo_latest_common_sg" {
+  value = aws_security_group.mongo_latest_common
+}
+
+output "aws_clive_emr_launcher_lambda" {
+  value = aws_lambda_function.mongo_latest_emr_launcher
+}
