4e308832f16a47a6975ee30a3a099dadc33dbc8c
steveburton4 <Steven.burton@hotmail.co.uk>
2021-05-14 14:43:55 +0100

    Fix output name (#10)

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "mongo_latest_common_sg" {
   value = aws_security_group.mongo_latest_common
 }
 
-output "aws_clive_emr_launcher_lambda" {
+output "mongo_latest_emr_launcher_lambda" {
   value = aws_lambda_function.mongo_latest_emr_launcher
 }
