401624ebc35cf320e191a076569f8f537acb6925
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-06-21 11:32:41 +0100

    LLAP files

---
+++ s3_files.tf
@@ -0,0 +1,35 @@
+resource "aws_s3_bucket_object" "hadoop_lzo_jar" {
+  bucket = data.terraform_remote_state.common.outputs.config_bucket.id
+  key    = "emr/mongo_latest/files/llap/encryption/hadoop-lzo.jar"
+  content = file("${path.module}/files/llap/encryption/hadoop-lzo.jar")
+}
+
+resource "aws_s3_bucket_object" "hadoop_lzo_0_4_19_jar" {
+  bucket = data.terraform_remote_state.common.outputs.config_bucket.id
+  key    = "emr/mongo_latest/files/llap/encryption/hadoop-lzo-0.4.19.jar"
+  content = file("${path.module}/files/llap/encryption/hadoop-lzo-0.4.19.jar")
+}
+
+resource "aws_s3_bucket_object" "libgplcompression_a" {
+  bucket = data.terraform_remote_state.common.outputs.config_bucket.id
+  key    = "emr/mongo_latest/files/llap/encryption/native/libgplcompression.a"
+  content = file("${path.module}/files/llap/encryption/native/libgplcompression.a")
+}
+
+resource "aws_s3_bucket_object" "libgplcompression_so" {
+  bucket = data.terraform_remote_state.common.outputs.config_bucket.id
+  key    = "emr/mongo_latest/files/llap/encryption/native/libgplcompression.so"
+  content = file("${path.module}/files/llap/encryption/native/libgplcompression.so")
+}
+
+resource "aws_s3_bucket_object" "libgplcompression_so_0" {
+  bucket = data.terraform_remote_state.common.outputs.config_bucket.id
+  key    = "emr/mongo_latest/files/llap/encryption/native/libgplcompression.so.0"
+  content = file("${path.module}/files/llap/encryption/native/libgplcompression.so.0")
+}
+
+resource "aws_s3_bucket_object" "libgplcompression_so_0_0_0" {
+  bucket = data.terraform_remote_state.common.outputs.config_bucket.id
+  key    = "emr/mongo_latest/files/llap/encryption/native/libgplcompression.so.0.0.0"
+  content = file("${path.module}/files/llap/encryption/native/libgplcompression.so.0.0.0")
+}
