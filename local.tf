acfbde8285bb10f631946df7fc7db7321a808d1f
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-08-26 12:58:46 +0100

    Latest PT

--- local.tf
+++ local.tf
@@ -119,11 +119,11 @@ locals {
   }
 
   payment_timelines_version = {
-    development = "0.0.17"
-    qa          = "0.0.17"
-    integration = "0.0.17"
-    preprod     = "0.0.17"
-    production  = "0.0.17"
+    development = "0.0.18"
+    qa          = "0.0.18"
+    integration = "0.0.18"
+    preprod     = "0.0.18"
+    production  = "0.0.18"
   }
 
   cbol_data_version = {
