28d13590763e5ac394aaedcd9fb24a871b49e141
Nathan Bigaignon <nathan.bigaignon@engineering.digital.dwp.gov.uk>
2022-07-25 16:44:58 +0100

    Add additional Slack notifications around CBOL steps (#65)

--- cloudwatch-events.tf
+++ cloudwatch-events.tf
@@ -122,6 +122,98 @@ resource "aws_cloudwatch_event_rule" "mongo_latest_running" {
 EOF
 }
 
+resource "aws_cloudwatch_event_rule" "cbol_step_running" {
+  name          = "cbol_step_running"
+  description   = "checks that cbol step is running"
+  event_pattern = <<EOF
+{
+  "source": [
+    "aws.emr"
+  ],
+  "detail-type": [
+    "EMR Step Status Change"
+  ],
+  "detail": {
+    "state": [
+      "RUNNING"
+    ],
+    "name": [
+      "cbol-sql"
+    ]
+  }
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_rule" "cbol_step_success" {
+  name          = "cbol_step_success"
+  description   = "checks that cbol step has completed without errors"
+  event_pattern = <<EOF
+{
+  "source": [
+    "aws.emr"
+  ],
+  "detail-type": [
+    "EMR Step Status Change"
+  ],
+  "detail": {
+    "state": [
+      "COMPLETED"
+    ],
+    "name": [
+      "cbol-report"
+    ]
+  }
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_rule" "cbol_sql_step_failed" {
+  name          = "cbol_sql_step_failed"
+  description   = "checks if cbol-sql step has failed"
+  event_pattern = <<EOF
+{
+  "source": [
+    "aws.emr"
+  ],
+  "detail-type": [
+    "EMR Step Status Change"
+  ],
+  "detail": {
+    "state": [
+      "FAILED"
+    ],
+    "name": [
+      "cbol-sql"
+    ]
+  }
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_rule" "cbol_report_step_failed" {
+  name          = "cbol_report_step_failed"
+  description   = "checks if cbol-report step has failed"
+  event_pattern = <<EOF
+{
+  "source": [
+    "aws.emr"
+  ],
+  "detail-type": [
+    "EMR Step Status Change"
+  ],
+  "detail": {
+    "state": [
+      "FAILED"
+    ],
+    "name": [
+      "cbol-report"
+    ]
+  }
+}
+EOF
+}
+
 resource "aws_cloudwatch_metric_alarm" "mongo_latest_failed" {
   count                     = local.mongo_latest_alerts[local.environment] == true ? 1 : 0
   alarm_name                = "mongo_latest_failed"
@@ -246,6 +338,110 @@ resource "aws_cloudwatch_metric_alarm" "mongo_latest_running" {
     local.common_tags,
     {
       Name              = "mongo_latest_running",
+      notification_type = "Information",
+      severity          = "Critical"
+    },
+  )
+}
+
+resource "aws_cloudwatch_metric_alarm" "cbol_step_running" {
+  count                     = local.mongo_latest_alerts[local.environment] == true ? 1 : 0
+  alarm_name                = "cbol_step_running"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "TriggeredRules"
+  namespace                 = "AWS/Events"
+  period                    = "60"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  alarm_description         = "Monitoring execution of CBOL steps"
+  insufficient_data_actions = []
+  alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
+  dimensions = {
+    RuleName = aws_cloudwatch_event_rule.cbol_step_running.name
+  }
+  tags = merge(
+    local.common_tags,
+    {
+      Name              = "cbol_step_running",
+      notification_type = "Information",
+      severity          = "Critical"
+    },
+  )
+}
+
+resource "aws_cloudwatch_metric_alarm" "cbol_step_success" {
+  count                     = local.mongo_latest_alerts[local.environment] == true ? 1 : 0
+  alarm_name                = "cbol_step_success"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "TriggeredRules"
+  namespace                 = "AWS/Events"
+  period                    = "60"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  alarm_description         = "Monitoring of CBOL steps successful completion"
+  insufficient_data_actions = []
+  alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
+  dimensions = {
+    RuleName = aws_cloudwatch_event_rule.cbol_step_success.name
+  }
+  tags = merge(
+    local.common_tags,
+    {
+      Name              = "cbol_step_success",
+      notification_type = "Information",
+      severity          = "Critical"
+    },
+  )
+}
+
+resource "aws_cloudwatch_metric_alarm" "cbol_sql_step_failed" {
+  count                     = local.mongo_latest_alerts[local.environment] == true ? 1 : 0
+  alarm_name                = "cbol_sql_step_failed"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "TriggeredRules"
+  namespace                 = "AWS/Events"
+  period                    = "60"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  alarm_description         = "Monitoring of cbol-sql step failure"
+  insufficient_data_actions = []
+  alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
+  dimensions = {
+    RuleName = aws_cloudwatch_event_rule.cbol_sql_step_failed.name
+  }
+  tags = merge(
+    local.common_tags,
+    {
+      Name              = "cbol_sql_step_failed",
+      notification_type = "Information",
+      severity          = "Critical"
+    },
+  )
+}
+
+resource "aws_cloudwatch_metric_alarm" "cbol_report_step_failed" {
+  count                     = local.mongo_latest_alerts[local.environment] == true ? 1 : 0
+  alarm_name                = "cbol_report_step_failed"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "TriggeredRules"
+  namespace                 = "AWS/Events"
+  period                    = "60"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  alarm_description         = "Monitoring of cbol-report step failure"
+  insufficient_data_actions = []
+  alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
+  dimensions = {
+    RuleName = aws_cloudwatch_event_rule.cbol_report_step_failed.name
+  }
+  tags = merge(
+    local.common_tags,
+    {
+      Name              = "cbol_report_step_failed",
       notification_type = "Information",
       severity          = "Critical"
     },
