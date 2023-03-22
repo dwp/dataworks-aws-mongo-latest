7e75ed0e03fcd29a81d2d08df0d52181b57553ec
sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>
2023-03-22 12:34:48 +0000

    adds more subnet ids for emr config

--- cluster_config.tf
+++ cluster_config.tf
@@ -29,14 +29,10 @@ resource "aws_s3_object" "instances" {
   key    = "emr/mongo_latest/instances.yaml"
   content = templatefile("${path.module}/cluster_config/instances.yaml.tpl",
     {
-      keep_cluster_alive = local.keep_cluster_alive[local.environment]
-      add_master_sg      = aws_security_group.mongo_latest_common.id
-      add_slave_sg       = aws_security_group.mongo_latest_common.id
-      subnet_id = (
-        local.use_capacity_reservation[local.environment] == true ?
-        data.terraform_remote_state.internal_compute.outputs.mongo_latest_subnet.subnets[index(data.terraform_remote_state.internal_compute.outputs.mongo_latest_subnet.subnets.*.availability_zone, data.terraform_remote_state.common.outputs.ec2_capacity_reservations.emr_m5_16_x_large_2a.availability_zone)].id :
-        data.terraform_remote_state.internal_compute.outputs.mongo_latest_subnet.subnets[index(data.terraform_remote_state.internal_compute.outputs.mongo_latest_subnet.subnets.*.availability_zone, local.emr_subnet_non_capacity_reserved_environments)].id
-      )
+      keep_cluster_alive                  = local.keep_cluster_alive[local.environment]
+      add_master_sg                       = aws_security_group.mongo_latest_common.id
+      add_slave_sg                        = aws_security_group.mongo_latest_common.id
+      subnet_ids                          = data.terraform_remote_state.internal_compute.outputs.mongo_latest_subnet.subnets.*.id
       master_sg                           = aws_security_group.mongo_latest_master.id
       slave_sg                            = aws_security_group.mongo_latest_slave.id
       service_access_sg                   = aws_security_group.mongo_latest_emr_service.id
