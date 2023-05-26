aabacf485e99aea6722abb40514fced19e3c51e2
Pjay <38280665+gbpjay@users.noreply.github.com>
2023-05-26 15:38:41 +0100

    Update local.tf
    Changing Tez sessions per queue  for stats in prod
    
    Signed-off-by: Pjay <38280665+gbpjay@users.noreply.github.com>

--- local.tf
+++ local.tf
@@ -226,8 +226,8 @@ locals {
     development = "5"
     qa          = "5"
     integration = "5"
-    preprod     = "35"
-    production  = "10"
+    preprod     = "20"
+    production  = "20"
   }
 
   use_capacity_reservation = {
