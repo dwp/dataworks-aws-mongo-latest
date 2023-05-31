adaba2023af7ec2fd66cebaa08b9bee0e085980f
Pjay <38280665+gbpjay@users.noreply.github.com>
2023-05-31 13:20:21 +0100

    Dtwrks 154 v5 (#116)
    * Update configurations.yaml.tpl
    
    turned vectorisation setting false
    
    Signed-off-by: Pjay <38280665+gbpjay@users.noreply.github.com>
    
    * Update local.tf
    
    increase tez sessions a bit
    
    Signed-off-by: Pjay <38280665+gbpjay@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Pjay <38280665+gbpjay@users.noreply.github.com>

--- local.tf
+++ local.tf
@@ -226,8 +226,8 @@ locals {
     development = "5"
     qa          = "5"
     integration = "5"
-    preprod     = "20"
-    production  = "20"
+    preprod     = "25"
+    production  = "25"
   }
 
   use_capacity_reservation = {
