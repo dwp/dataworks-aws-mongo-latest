b337c0004058a407524595d6b2999153d9592f17
Pjay <38280665+gbpjay@users.noreply.github.com>
2023-06-08 16:09:15 +0100

    Revert "Update local.tf (#121)"
    This reverts commit a936a14e24a5712a52b4cf42ff42aa341d49e9b3.

--- local.tf
+++ local.tf
@@ -193,8 +193,8 @@ locals {
     development = "1024"
     qa          = "1024"
     integration = "1024"
-    preprod     = "8192"
-    production  = "8192"
+    preprod     = "7168"
+    production  = "7168"
   }
 
   # 0.8 of tez_am_resource_memory_mb
@@ -202,24 +202,24 @@ locals {
     development = "-Xmx816m"
     qa          = "-Xmx816m"
     integration = "-Xmx816m"
-    preprod     = "-Xmx6554m"
-    production  = "-Xmx6554m"
+    preprod     = "-Xmx5734m"
+    production  = "-Xmx5734m"
   }
 
   tez_task_resource_memory_mb = {
     development = "2048"
     qa          = "2048"
     integration = "2048"
-    preprod     = "6554"
-    production  = "6554"
+    preprod     = "5734"
+    production  = "5734"
   }
 
   hive_max_reducers = {
     development = "1099"
     qa          = "1099"
     integration = "1099"
-    preprod     = "5000"
-    production  = "5000"
+    preprod     = "3000"
+    production  = "3000"
   }
 
   hive_tez_sessions_per_queue = {
